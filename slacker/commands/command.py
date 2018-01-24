c9a1ecd6b2c27b0cef69cf9512d53ed304348276
Morten Kristensen <msk@nullpointer.dk>
2018-01-25 00:33:56 +0100

    Fixed #2: Abstract Command base class

---
+++ slacker/commands/command.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+  """Command encapsulates a Slacker command with a name, description, help text, alises, and the
+  action to perform.
+  """
+
+  @abstractmethod
+  def name(self):
+    """Returns the name of the command. This is the actual command, like 'download'."""
+    pass
+
+  @abstractmethod
+  def description(self):
+    """Returns the description of the command."""
+    pass
+
+  @abstractmethod
+  def help(self):
+    """Returns the help text of the command."""
+    pass
+
+  def aliases(self):
+    """Returns a list of aliases defined for this command."""
+    return None
+
+  @abstractmethod
+  def action(self):
+    """Executes the action of the command."""
+    pass
