c086509d6042583214ec2c7131843dda0c072920
Morten Kristensen <msk@nullpointer.dk>
2018-01-25 19:51:51 +0100

    Implemented command Registrar class

---
+++ slacker/commands/registrar.py
@@ -0,0 +1,54 @@
+class Registrar:
+  """Registrar keeps commands that have been registered with it."""
+
+  def __init__(self):
+    self.__commands = {} # command name -> command instance
+    self.__aliases = {} # alias -> command name
+
+  def register(self, command):
+    """Registers a command by name and aliases."""
+
+    # Add command.
+    if command.name() in self.__commands:
+      raise ValueError("Command already registered: {}".format(command.name()))
+    self.__commands[command.name()] = command
+
+    # Add aliases.
+    if not command.aliases():
+      return
+    for alias in command.aliases():
+      if alias in self.__commands:
+        raise ValueError("Alias '{}' of command '{}' already registered as command '{}'"
+                         .format(alias, command.name(), alias))
+      elif alias in self.__aliases:
+        other_cmd = self.__aliases[alias]
+        raise ValueError("Alias '{}' of command '{}' already registered for command '{}'"
+                         .format(alias, command.name(), other_cmd))
+      self.__aliases[alias] = command.name()
+
+  def find(self, name):
+    """Lookup command by name and aliases."""
+
+    if name in self.__commands:
+      return self.__commands[name]
+
+    if name in self.__aliases:
+      return self.__commands[self.__aliases[name]]
+
+    return None
+
+  def count(self):
+    """Returns the amount of commands registered."""
+    return len(self.__commands)
+
+  def commands(self):
+    """Returns all registered command instances."""
+    return self.__commands.values()
+
+  def names(self):
+    """Returns all registered command names."""
+    return self.__commands.keys()
+
+  def aliases(self):
+    """Returns all registered command aliases."""
+    return self.__aliases.keys()
