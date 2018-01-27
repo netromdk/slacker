8bdda11a4520ff0657a0069aa3d054193c5fcf9e
Morten Kristensen <msk@nullpointer.dk>
2018-01-27 19:57:03 +0100

    Fixed indentation in help_command.py

--- slacker/commands/help_command.py
+++ slacker/commands/help_command.py
@@ -2,21 +2,21 @@ from .command import Command
 
 class HelpCommand(Command):
   def name(self):
-      return "help"
+    return "help"
 
   def description(self):
-      return "Displays general help."
+    return "Displays general help."
 
   def aliases(self):
-      return ["h"]
+    return ["h"]
 
   def __show(self, cmd):
-      aliases = ""
-      if cmd.aliases():
-          aliases = " [{}]".format(", ".join(cmd.aliases()))
+    aliases = ""
+    if cmd.aliases():
+      aliases = " [{}]".format(", ".join(cmd.aliases()))
       print("  {}{}:\t\t{}".format(cmd.name(), aliases, cmd.description()))
 
   def action(self, args = None):
-      print("Displaying available commands:")
-      for cmd in Command.find_all():
-        self.__show(cmd())
+    print("Displaying available commands:")
+    for cmd in Command.find_all():
+      self.__show(cmd())
