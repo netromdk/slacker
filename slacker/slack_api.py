34a58de9fb5abb4633443a78d8ac952c3a2428b6
Morten Kristensen <msk@nullpointer.dk>
2018-02-04 01:23:47 +0100

    Let token be overridable and accept no arguments also

--- slacker/slack_api.py
+++ slacker/slack_api.py
@@ -6,13 +6,16 @@ from slacker.environment.config import Config
 class SlackAPI:
   """Encapsulates sending requests to the Slack API and getting back JSON responses."""
 
-  def __init__(self):
-    self.__token = Config.get().active_workspace_token()
+  def __init__(self, token = None):
+    if token:
+      self.__token = token
+    else:
+      self.__token = Config.get().active_workspace_token()
 
     # Methods that doesn't require the token to be sent.
     self.__token_unneeded = ('api.test',)
 
-  def post(self, method, args):
+  def post(self, method, args = {}):
     """Send HTTP POST using method, as the part after https://slack.com/api/, and arguments as a
     dictionary of arguments."""
     url = 'https://slack.com/api/{}'.format(method)
