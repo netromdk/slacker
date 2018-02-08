9f8ba83e452887988a442c6e0fa9192fda80840d
Morten Kristensen <msk@nullpointer.dk>
2018-02-08 22:13:59 +0100

    Verify Python v3+ when running script

--- slacker.py
+++ slacker.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+# Make sure v3+ is used.
+import sys
+vinfo = sys.version_info
+if vinfo.major < 3:
+  print('You are using Python v{}.{}. v3+ is required!'.format(vinfo.major, vinfo.minor))
+  sys.exit(-1)
+
 from slacker.main import start_slacker
 
 if __name__ == '__main__':
