c8395a8191b9a44031a0af5f7f51583609e0f693
Morten Kristensen <msk@nullpointer.dk>
2018-02-11 12:20:07 +0100

    Chat post ephemeral message command
    Fixes #82.

--- slacker/commands/__init__.py
+++ slacker/commands/__init__.py
@@ -17,3 +17,4 @@ from .emoji import emoji_list_command
 from .channels import channels_list_command
 from .users import users_list_command
 from .chat import chat_postmessage_command
+from .chat import chat_postephemeral_command
---
+++ slacker/commands/chat/chat_postephemeral_command.py
@@ -0,0 +1,30 @@
+from slacker.commands.command import Command
+from slacker.commands.argument_parser import ArgumentParser
+from slacker.slack_api import SlackAPI
+
+class ChatPostEphemeralCommand(Command):
+  def name(self):
+    return "chat.postephemeral"
+
+  def description(self):
+    return "Post ephemeral message to a channel on Slack that is only visible to assigned user."
+
+  def make_parser(self):
+    parser = ArgumentParser(prog = self.name(), description = self.description())
+    parser.add_argument('-c', '--channel', type = str, help = "Channel ID to post to.")
+    parser.add_argument('-u', '--user', type = str, help = "User ID to post ephemeral message to.")
+    parser.add_argument('--as-user', action = 'store_true',
+                        help = "Post as authed user instead of as bot.")
+    parser.add_argument('text', type = str, help = "Text to post.")
+    return parser
+
+  def action(self, args = None):
+    channel = args.channel
+    user = args.user
+    text = args.text
+    self.logger.debug('Sending ephemeral message to {} in {}: {}'.format(user, channel, text))
+    SlackAPI().post('chat.postEphemeral',
+                    {'channel': channel,
+                     'user': user,
+                     'text': text,
+                     'as_user': args.as_user})
