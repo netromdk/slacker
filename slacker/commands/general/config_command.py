7d4a5659533e16470fd1dcf69f68224222973442
Morten Kristensen <msk@nullpointer.dk>
2018-02-11 18:44:29 +0100

    Show and modify read-only state via config command

--- slacker/commands/general/config_command.py
+++ slacker/commands/general/config_command.py
@@ -1,5 +1,7 @@
 import json
+
 from slacker.commands.command import Command
+from slacker.commands.argument_parser import ArgumentParser
 from slacker.environment.config import Config
 
 class ConfigCommand(Command):
@@ -9,7 +11,21 @@ class ConfigCommand(Command):
   def description(self):
     return "Shows current config state."
 
+  def make_parser(self):
+    parser = ArgumentParser(prog = self.name(), description = self.description())
+    parser.add_argument("--set-read-only", dest = "read_only", action = 'store_true',
+                        help = "Set read-only mode.")
+    parser.add_argument("--unset-read-only", dest = "read_only", action = 'store_false',
+                        help = "Unset read-only mode.")
+    parser.set_defaults(read_only = None)
+    return parser
+
   def action(self, args = None):
     config = Config.get()
+
+    if not args.read_only is None:
+      config.set_read_only(args.read_only)
+      config.save()
+
     self.logger.info("Current config state of '{}'".format(config.file_path()))
     self.logger.info(json.dumps(config.safe_dict(), indent = 2))
