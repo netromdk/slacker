24b2320173d8c82073d1818c69502846cd45727b
Morten Kristensen <msk@nullpointer.dk>
2018-02-18 00:33:17 +0100

    Remove version check and NOQA:E402

--- slacker.py
+++ slacker.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
-
-# Make sure v3+ is used.
-import sys
-vinfo = sys.version_info
-if vinfo < (3, 0):
-  print("Python v3+ is required! You are using v{}.{}.".format(vinfo.major, vinfo.minor))
-  sys.exit(-1)
-
-from slacker.main import start_slacker  # noqa: E402
+from slacker.main import start_slacker
 
 if __name__ == "__main__":
   start_slacker()
