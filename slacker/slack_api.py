6588caca68e76ed8095887ae3a85ed1f3a827aef
Brandon Davis <dbrandon.91@gmail.com>
2018-09-15 11:46:29 -0700

    Add support for GET Slack API methods

--- slacker/slack_api.py
+++ slacker/slack_api.py
@@ -22,6 +22,7 @@ class SlackAPI:
   def __init__(self, token=None, command=None, requires_token=False, is_destructive=True):
     config = Config.get()
     self.__logger = Logger(__name__).get()
+    self.__url = "https://slack.com/api/{}"
 
     if token:
       self.__token = token
@@ -32,37 +33,66 @@ class SlackAPI:
     self.__is_destructive = is_destructive if not command else command.is_destructive()
 
   def __check_read_only_abort(self, method):
+    """Returns exception to avoid sending requests to Slack API.
+
+    If the command is marked as destructive or the REPL is in read-only mode
+    requests to Slack API will not be sent.
+
+    Arguments:
+      method {str} -- Slack API method name
+
+    Raises:
+      SlackAPIException
+    """
     if self.__is_destructive and Config.get().read_only():
-      raise SlackAPIException("Not executing '{}' due to read-only mode!".format(method))
+      raise SlackAPIException(
+        "Not executing '{}' due to read-only mode!".format(method))
 
   def post(self, method, args={}):
-    """Send HTTP POST using method, as the part after https://slack.com/api/, and arguments as a
-    dictionary of arguments."""
+    """Send HTTP POST request to Slack API.
+
+    Arguments:
+      method {str} -- Slack API method name
+
+    Keyword Arguments:
+      args {dict} -- Arguments required by Slack API method (default: {{}})
+
+    Returns:
+      dict -- Slack API response
+    """
     self.__check_read_only_abort(method)
 
-    url = "https://slack.com/api/{}".format(method)
+    url = self.__url.format(method)
     if self.__requires_token:
       args["token"] = self.__token
 
     response = requests.post(url, data=args)
-    if response.status_code != 200:
-      raise SlackAPIException("Unsuccessful API request: {} (code {})\nReason: {}\nResponse: {}"
-                              .format(response.url, response.status_code, response.reason,
-                                      response.text))
+    self.__validate_response(response)
 
-    data = response.json()
-    if "ok" not in data:
-      raise SlackAPIException("Unsuccessful API request: {}\nInvalid response: {}"
-                              .format(response.url, data))
+    return response.json()
 
-    if not data["ok"]:
-      error = ""
-      if "error" in data:
-        error = data["error"]
-      raise SlackAPIException("Unsuccessful API request: {}\nError: {}".format(response.url, error),
-                              error)
+  def get(self, method, args={}):
+    """Send HTTP GET request to Slack API.
 
-    return data
+    Arguments:
+      method {string} -- Slack API method name
+
+    Keyword Arguments:
+      args {dict} -- Arguments required by Slack API method (default: {{}})
+
+    Returns:
+      dict -- Slack API response
+    """
+    self.__check_read_only_abort(method)
+
+    url = self.__url.format(method)
+    if self.__requires_token:
+      args["token"] = self.__token
+
+    response = requests.get(url, params=args)
+    self.__validate_response(response)
+
+    return response.json()
 
   def download_file(self, file_id, folder):
     """Download file via ID to a folder. File IDs can be retrieved using the `files.list'
@@ -92,3 +122,29 @@ class SlackAPI:
         f.write(chunk)
 
     return file_name
+
+  def __validate_response(self, response):
+    """Check Slack API response for errors
+
+    Arguments:
+      response {requests.Response} -- [description]
+
+    Raises:
+      SlackAPIException
+    """
+    if response.status_code != 200:
+      raise SlackAPIException("Unsuccessful API request: {} (code {})\nReason: {}\nResponse: {}"
+                              .format(response.url, response.status_code, response.reason,
+                                      response.text))
+
+    data = response.json()
+    if "ok" not in data:
+      raise SlackAPIException("Unsuccessful API request: {}\nInvalid response: {}"
+                              .format(response.url, data))
+
+    if not data["ok"]:
+      error = ""
+      if "error" in data:
+        error = data["error"]
+      raise SlackAPIException("Unsuccessful API request: {}\nError: {}"
+                              .format(response.url, error), error)
